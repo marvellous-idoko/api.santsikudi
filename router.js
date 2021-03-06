7a43040cab499798c1d509efc2d818b528ad3f06
unknown <idokomarvelous@gmail.com>
2021-03-06 12:25:40 +0100

    sys upd

--- router.js
+++ router.js
@@ -184,7 +184,8 @@ router.post('/withdrawal', async (req, res) => {
         console.error(ex);
     }
 })
-var acctNoToTransferTo;  var u;
+        var uPin;
+        var acctNoToTransferTo;  var u;
 router.post('/ussd', async (req, res) => {
     let { sessionId, serviceCode, phoneNumber, text } = req.body;
     // console.log(req.body)
@@ -354,12 +355,55 @@ router.post('/ussd', async (req, res) => {
                             res.send(response)
                         }
         else if(s =='1*2') {
-            console.info('trashpkpkp')
+            
             let response =  `END Here is you account balance ${u.acctBalance}
             Have a nice day`
             res.send(response)
         }
-          
+        else if(s =='1*5') {
+            let response
+            var u = await ussd.findOne({contact: phoneNumber.toString().slice(1,13)})
+            if (u['pin'] == null || undefined){
+                response = `CON set a four digits pin e.g 1234`
+                res.send(response)
+            }else{
+                response = `CON pin already set
+                press 1 to set new pin`
+                res.send(response)
+            }
+        }
+        else if (s =='1*5*1'){
+            let response = `CON input old pin`
+            res.send(response)
+            
+        }
+        else if(s == '1*5*1*' + s.slice(6,9)){
+            let response
+            var b = await ussd.findOne({contact:phoneNumber.toString().slice(1,13)})
+            if (b.pin != s.slice(5,8)){
+                response = `END wrong pin, try again later` 
+                res.send(response)
+            }else{
+                response = `CON Input new pin e.g 1234`
+                res.send(response)
+            }
+        }
+        else if(s == '1*5*1*' + s.slice(6,9) + '*'+s.slice(11,14)){
+            var b = await ussd.findOne({contact:phoneNumber.toString().slice(1,13)})
+            b.pin = s.slice(11,14);
+            var finished = await b.save()
+            let response = `END Your pin has successfully be set ${finished.pin}`
+            res.send(response)
+                 
+        }
+            else if (s =='1*5*'+s.slice(4,7)){
+                var b = await ussd.findOne({contact:phoneNumber.toString().slice(1,13)})
+                b.pin = s.slice(4,7);
+                var finished = await b.save()
+                let response = `END Your pin has successfully be set ${finished.pin}`
+                res.send(response)
+            }
+            
         else if(s =='1*3') {
             let response = `CON select amount to save
             1. 1,000.00
